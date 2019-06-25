2621fed01724f6d472a2a771a969eb0456a7ccce
marleciooliveira <marleciooliveira@gmail.com>
2019-06-25 11:52:36 +0000

    Commit message

--- index.php
+++ index.php
@@ -3,6 +3,17 @@ echo "<p> ======================================================================
 echo "<p> ============================================Prova de Conceito Comdono ============================================ <p>" ;
 echo "<p> ===================================================================================================================<p>" ;
 
+
+
+echo "<p>log de erros do apache <p>"
+$output = shell_exec('cat /var/log/apache2/error.log');
+echo "<pre>$output</pre>";
+
+
+
+
+echo "<p> relatório das features do PHP <p>"
+
 $output = shell_exec('php -i');
 echo "<pre>$output</pre>";
 
