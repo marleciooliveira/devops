f7b128ad44fafe5e4b4c7b839d65bfde1929c799
marleciooliveira <marleciooliveira@gmail.com>
2019-06-25 13:22:32 +0000

    Commit message

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ echo "<p> ======================================================================
 echo "<p> ============================================Prova de Conceito Comdono ============================================ <p>" ;
 echo "<p> ===================================================================================================================<p>" ;
 
+echo "<p> Steps para melhorias em segurança da aplicação: <p>" ;
+echo "<p> 1 -  Criar Vhosts forçando as conexões a passarem pelo apache permitindo o controle e segurança das sessões dos usuários e permitindo o uso do apache modsecurity ou phpids : /jenkins ==> url(8080/jenkins) <p>" ;
+echo "<p> 2 - Criar Vhosts para implementação de certificação digital:  SSl sobre http (HTTPS) <p> " ;
+echo "<p> 3 - 
+
+
 
 
 echo "<p> relatório das features do PHP <p>" ;
